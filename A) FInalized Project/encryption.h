b9fbbd63bd225ff84e0fc3f35ae7c3146db28fb4
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-04-17 22:08:29 +0500

    Update encryption.h

--- A) FInalized Project/encryption.h
+++ A) FInalized Project/encryption.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-// Base class for encryption
+// Base class for encryption is also an Abstract class due to these Pure Virtual Function
 class Encryption {
 public:
     virtual void encrypt(const string &filename) = 0; // Pure virtual function for encryption
@@ -14,46 +14,38 @@ public:
 
 };
 
-// Reverse-based encryption
-class ReverseEncryption : public Encryption {
-public:
-    void encrypt(const string &filename) override;
-    void decrypt(const string &filename) override;
-};
-
 // Vigenère cipher encryption child class of Encryption
 class VigenereEncryption : public Encryption {
     private:
-        string generateRandomKey();  // Generates a random encryption key for Vigenère cipher
+        string generateRandomKey();     // Generates a random encryption key for Vigenère cipher
     public:
-        //VigenereEncryption(const string &keyword);
-        string generateKey(const string& text, const string& keyword);  // Generates a repeating key based on the file content
-        void encrypt(const string &filename) override;  // Overrides encrypt function for Vigenère cipher
-        void decrypt(const string &filename) override;  // Overrides decrypt function for Vigenère cipher
-        void storeKey(const string &filename, const string &key); // Stores the encryption key in a file named "keylog.txt"
-        string retrieveKey(const string &filename); // Retrieves the stored encryption key from "keylog.txt"
+        string generateKey(const string& text, const string& keyword);  // Generates a repeating key based wrrpaing around the file content
+        void encrypt(const string &filename) override;                  // Overrides encrypt function for Vigenère cipher
+        void decrypt(const string &filename) override;                  // Overrides decrypt function for Vigenère cipher
+        void storeKey(const string &filename, const string &key);       // Stores the encryption key in a file named "keylog.txt"
+        string retrieveKey(const string &filename);                     // Retrieves the stored encryption key from "keylog.txt"
 };
 
 // Ceaser cipher encryption child class of Encryption
 class CeaserCipher : public Encryption{
 	private:
-        string generateRandomKey();
+        string generateRandomKey();     // Generates a random encryption key for Ceaser cipher
     public:
-        void encrypt(const string &filename) override;
-        void decrypt(const string &filename) override;
-        void storeKey(const string &filename, const string &key); // Stores the encryption key in a file named "keylog.txt"
-        string retrieveKey(const string &filename); // Retrieves the stored encryption key from "keylog.txt"
+        void encrypt(const string &filename) override;              // Overrides encrypt function for Ceaser cipher
+        void decrypt(const string &filename) override;              // Overrides decrypt function for Ceaser cipher
+        void storeKey(const string &filename, const string &key);   // Stores the encryption key in a file named "keylog.txt"
+        string retrieveKey(const string &filename);                 // Retrieves the stored encryption key from "keylog.txt"
 };
 
 // Railfence Cipher encryption child class of encryption
 class RailFenceEncryption : public Encryption {
     private:
-        string generateRandomKey();
+        string generateRandomKey();      // Generates a random encryption key for Ceaser cipher
     public:
-        void storeKey(const string &filename, const string& key);
-        string retrieveKey(const string &filename);
-        void encrypt(const string &filename) override;
-        void decrypt(const string &filename) override;
+        void encrypt(const string &filename) override;              // Overrides encrypt function for RailFence cipher
+        void decrypt(const string &filename) override;              // Overrides decrypt function for RailFence cipher
+        void storeKey(const string &filename, const string& key);   // Stores the encryption key in a file named "keylog.txt"
+        string retrieveKey(const string &filename);                 // Retrieves the stored encryption key from "keylog.txt"
 };
 
 #endif // ENCRYPTION_H
