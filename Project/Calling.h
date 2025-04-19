c92cc0a9e8da385aac97c45ed6ea19bbf647338d
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-04-19 11:17:26 +0500

    Delete Project directory

--- Project/Calling.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALLING_H
-#define CALLING_H
-
-#include <string>
-
-// This is the function that will call encrypt and decrypt Process
-void RunEncryptionHandler(int Number);
-
-#endif // CALLING_H
