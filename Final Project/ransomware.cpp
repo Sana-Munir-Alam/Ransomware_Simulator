f264662017799faa74bcaab762f8b22d18526610
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-04-21 19:02:05 +0500

    Update ransomware.cpp

--- Final Project/ransomware.cpp
+++ Final Project/ransomware.cpp
@@ -8,23 +8,22 @@ using namespace std;
 static string GeneratedPassword = "";
 
 string GenerateRandomPassword() {
-    const string charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    const int length = 5;
-    string password = "";
+    const string charset = "0123456789abcdefghijklmnopqrstuvwxyz";  // Allowed characters for the Password
+    const int length = 5;                                           // Length of the Password.
+    string password = "";                                           // Variable to store in the password.
 
-    for (int i = 0; i < length; ++i) {
-        int index = rand() % charset.length();
-        password += charset[index];
+    for (int i = 0; i < length; ++i) {          // Loop to randomly pick 5 characters from the charset
+        int index = rand() % charset.length();  // Generate a random index which will be used to select the characters for the password
+        password += charset[index];             // Append the character to the password
     }
-
-    GeneratedPassword = password;  // Store it for later verification
-    return password;
+    GeneratedPassword = password;               // Store the password for verification later
+    return password;                            // Return the new password
 }
 
 bool VerifyPassword(const string& password) {
-    return password == GeneratedPassword;  // Compare with the correct password
+    return password == GeneratedPassword;       // Compares a given password with the previously generated one. If it matches return TRUE else FALSE
 }
 
 string GetGeneratedPassword() {
-    return GeneratedPassword;
+    return GeneratedPassword;                   // Returns the currently stored/generated password (for testing/debugging).
 }
