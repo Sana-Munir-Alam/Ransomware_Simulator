2e59f8e3808b6c6697ea7035e2383a2f76ee6884
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-04-21 19:02:24 +0500

    Update ransomware.h

--- Final Project/ransomware.h
+++ Final Project/ransomware.h
@@ -3,8 +3,8 @@
 #include <string>
 using namespace std;
 
-string GenerateRandomPassword();
-bool VerifyPassword(const string& password);
-string GetGeneratedPassword();
+string GenerateRandomPassword();                // Generates a random 5-character password using numbers and lowercase letters.
+bool VerifyPassword(const string& password);    // Compares a given password with the previously generated one.
+string GetGeneratedPassword();                  // Returns the currently stored/generated password (for testing/debugging).
 
 #endif
