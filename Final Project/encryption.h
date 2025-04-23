8c3a5eaa5d82763bcd6183f7e940460fc0c39a75
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-04-23 09:43:07 +0500

    Update encryption.h

--- Final Project/encryption.h
+++ Final Project/encryption.h
@@ -41,10 +41,8 @@ class CeaserCipher : public Encryption{
     public:
         void encryptTXT(const string &filename) override;               // Overrides encrypt function for Ceaser cipher Text File
         void decryptTXT(const string &filename) override;               // Overrides decrypt function for Ceaser cipher Text File
-
         void encryptNON(const string &filename) override;               // Overrides encrypt function for Ceaser cipher Non-Text File
         void decryptNON(const string &filename) override;               // Overrides decrypt function for Ceaser cipher Non-Text File
-
         void storeKey(const string &filename, const string &key);       // Stores the encryption key in a file named "keylog.txt"
         string retrieveKey(const string &filename);                     // Retrieves the stored encryption key from "keylog.txt"
 };
@@ -52,16 +50,29 @@ class CeaserCipher : public Encryption{
 // Railfence Cipher encryption child class of encryption
 class RailFenceEncryption : public Encryption {
     private:
-        string generateRandomKey();      // Generates a random encryption key for Ceaser cipher
+        string generateRandomKey();      // Generates a random encryption key for Railfence cipher
     public:
         void encryptTXT(const string &filename) override;               // Overrides encrypt function for RailFence cipher Text File
         void decryptTXT(const string &filename) override;               // Overrides decrypt function for RailFence cipher Text File
-
         void encryptNON(const string &filename) override;               // Overrides encrypt function for RailFence cipher Non-Text File
-        void decryptNON(const string &filename) override;               // Overrides encrypt function for RailFence cipher Non-Text File
-
+        void decryptNON(const string &filename) override;               // Overrides decrypt function for RailFence cipher Non-Text File
         void storeKey(const string &filename, const string& key);       // Stores the encryption key in a file named "keylog.txt"
         string retrieveKey(const string &filename);                     // Retrieves the stored encryption key from "keylog.txt"
 };
 
+// Affine Cipher encryption child class of encryption
+class AffineEncryption : public Encryption {
+    private:
+        string generateTextKey();       // Generates a random encryption key for Affine cipher [Text-File]
+        string generateBinaryKey();     // Generates a random encryption key for Affine cipher [Non-Text File]
+        bool isCoprime(int a, int m);   // Function to check if two numbers 'a' and 'm' are coprime (i.e., GCD(a, m) == 1)
+        int modInverse(int a, int m);  
+    public:
+        void encryptTXT(const string &filename) override;               // Overrides encrypt function for Affine cipher Text File               
+        void decryptTXT(const string &filename) override;               // Overrides decrypt function for Affine cipher Text File               
+        void encryptNON(const string &filename) override;               // Overrides encrypt function for Affine cipher Non-Text File               
+        void decryptNON(const string &filename) override;               // Overrides decrypt function for Affine cipher Non-Text File
+        void storeKey(const string &filename, const string &key);       // Stores the encryption key in a file named "keylog.txt"       
+        string retrieveKey(const string &filename);                     // Retrieves the stored encryption key from "keylog.txt"                    
+    };
 #endif // ENCRYPTION_H
